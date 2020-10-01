ff8395f5b65460433d5bf1589cfefcafdcf1aaba
theoddartisan <69279291+theoddartisan@users.noreply.github.com>
2020-10-01 09:10:31 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # ChromeMcMasterExtension
-This is an extension for Google Chrome that allows for quick access to McMaster-Carr's online catalog with some fun features. This is a fan made project and is not affiliated with McMaster-Carr. 
+This is a browser extension that allows for quick access to McMaster-Carr's online catalog with some fun features. This is a fan made project and is not affiliated with McMaster-Carr. 
 
 Main Features:
 - Random-view a random set of products in the McMaster catalog with the click of a button (This references an array of URLs pulled from one of McMaster's sitemap XML files with over 57,000 unique URLs for browsing their online catalog. While this doesn't get you the resolution that individual part numbers would, it's still a blast to see the vast scope of product offerings from McMaster this way.)
 - Search-search for a product or part number directly in the extension
-- CAD Download-enter a part number and automatically download the desired filetype
-- Make Label-enter a part number and automatically generate a label for that part that you can print
+- CAD Download-enter a part number and automatically download the desired filetype, file names can be customized to be more useful than the default names
+- Make Label-enter a part number and automatically generate a label for that part that you can print, with label formats for laser/inkjet and thermal printers
+- Part Detail Table-copy an automatically formatted table with part detail information from a product page, for use with creating purchase orders and bill of material documents
 
 I have always enjoyed browsing through the vast catalog of industrial supply products that McMaster-Carr offers. I wanted a way to see the full range of product offerings they have and thought that a "random" button of some kind would be fun. As I started developing the extension for that task I decided to start adding more functionality like the search bar and label maker. I am no coding expert so there are likely many improvements that can be made to the code for efficiency and such - feel free to contribute to the project! I'm just happy to be having fun with the learning process. 
 
@@ -14,12 +15,20 @@ I have always enjoyed browsing through the vast catalog of industrial supply pro
 - Save browsing history with each use of the random or search feature. Currently when tabs are updated with new URLs the browsing history is not saved so you can't even go back to a previously viewed product page
 - Add an optional setting for the random feature to pick from a list of part numbers instead of referencing Sitemap URLs which are limited to larger part categories
 - Create a feature to browse through products recently added to the catalog
-- Allow for CAD Downloading and Label Making features to accept a list of part numbers so that it can run through a batch without the user needing to manually enter each part number separately
 
 ## History
+v3.0.0: Released September 30, 2020
+- Added a Part Detail Table feature to allow users to quickly copy part information in a table format for insertion into purchase order forms or spreadsheets
+- Added a CAD file renaming feature to allow users to download CAD files with more useful names with options for how to customize words and characters in file names
+- Updated CAD Download feature to allow automatic downloading of CAD files for multiple parts from a comma-separated list of part numbers
+- Updated Label Making feature to allow automatic label making for multiple parts from a comma-separated list of part numbers
+- Added options to Label Making feature for selecting either photos or QR codes to be included on appropriately sized label formats
+
+v2.0.1: Released August 30, 2020
+- Small bug fixes
+
 v2.0.0: Released August 29, 2020
 - Updated interface to use HTML elements instead of image files for buttons
-- Set the 
 - Reduced filesize of URL reference list for random feature
 - Added a CAD Download feature that accepts a part number input and automatically navigates to the part page and downloads the selected filetype from the dropdown menu
 - Added a Label Making feature that accepts a part number input and automatically generates a PNG image file that can be printed as labels in various formats and sizes (that correspond with labels available through McMaster-Carr for purchase)
--- content.js
+++ content.js
@@ -1,11 +1,47 @@
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    //Collect part detail information from page and send to background
+    if (request.poInfoRequest == "poInfo") {
+        if (document.getElementsByClassName("bk attrComp").length == 0) {
+            sendResponse({partDetailCheck: "failed"});
+        }
+        else if (document.getElementsByClassName("PrceTxt")[0].innerText == "" || document.getElementsByClassName("mg mp mr ng ni spec-search--value").length > 1) {
+            sendResponse({partDetailCheck: "specifySize"});
+        }
+        else {
+            var poPartNumber = document.getElementsByClassName("bk attrComp")[0].innerText;
+            var mainTitle = document.getElementsByClassName("header-primary--pd")[0].innerText;
+            if (document.getElementsByClassName("header-secondary--pd")[0] == null) {
+                subTitle = "";
+            }
+            else {
+                var subTitle = document.getElementsByClassName("header-secondary--pd")[0].innerText;
+            };
+		    var poDescription = mainTitle + " " + subTitle;
+            var priceLine = document.getElementsByClassName("PrceTxt")[0].innerText;
+		    var poUnitPrice = priceLine.substr(0, priceLine.indexOf(" "));
+            if (document.getElementsByClassName("am eto--specs").length > 0) {
+                var poUnitQuantity = priceLine.substr(priceLine.indexOf(" ")+1) + ", [" + document.getElementsByClassName("mg mp mr ng ni spec-search--slctd spec-search--value")[0].innerText + "] "+ document.getElementsByClassName("AttrNm_Lbl Hide")[0].innerText;
+            }
+            else {
+		        var poUnitQuantity = priceLine.substr(priceLine.indexOf(" ")+1);
+            };
+            sendResponse({partDetailCheck: null,poPartNumber:poPartNumber, poDescription:poDescription, poUnitPrice:poUnitPrice, poUnitQuantity:poUnitQuantity});
+        };
+    };    
+
+    //Collect part number from part detail page if popup tool was activated with no value
     if (request.partnumberquery == "blankPartNum") {
         if (document.getElementsByClassName("bk attrComp").length == 0) {
             alert("Enter a Part Number or navigate to a Product Detail page and click again");                
             sendResponse({backtopopup: "wrong page"});
         }
         else if (request.query == "cad") {
+            if (location.href.search('-') != -1) {
+                partNumber = location.href.slice(location.href.indexOf('-')+1, location.href.length-1);
+            }
+            else {
             partNumber = document.getElementsByClassName("bk attrComp")[0].innerText;
+            };
             isthereCAD = document.getElementsByClassName("button-save--stacked button-tertiary--cad HideOnItmPrint save-cad").length;
             if (isthereCAD >0) {
             sendResponse({backtopopup: "carryon", partNumber:partNumber});
@@ -21,7 +57,7 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
         }
     };
     
-        
+    //Collect part information for creating labels
     if (request.command == "makelabel") {
         if (document.getElementsByClassName("WarningTxt").length > 0 || document.getElementsByClassName("jg jl").length > 0) {
             sendResponse({invalid: "invalid"});
@@ -47,7 +83,15 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
             sendResponse({next:'screenshot', mainTitle: mainTitle, subTitle:subTitle, partImage:partImage, partNum:partNum});
         };
     };
-                    
+    
+    //Wait for command from background to start the CAD file download
+    if (request.command == "downloadstart") {
+        var savefile = document.getElementsByClassName("button-save--stacked button-tertiary--cad HideOnItmPrint save-cad")[0];
+        setTimeout(function() {savefile.click();}, 100);
+        sendResponse();    
+    };
+
+    //Edit html of part detail page to prepare for CAD download, gather part information for CAD download
     if (request.command == "runDownload") {  
         var fileformat = request.filetype1;
         if (document.getElementsByClassName("WarningTxt").length > 0 || document.getElementsByClassName("jg jl").length > 0) {
@@ -55,7 +99,7 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
         }
         else if (document.getElementsByClassName("button-save--stacked button-tertiary--cad HideOnItmPrint save-cad").length !== 1) {
             alert("This part does not have CAD available.");
-            sendResponse({wrapup: "finished"});
+            sendResponse();
         }
         else {
             //eliminate the CAD option that is selected by default based on user's past CAD download selection
@@ -70,7 +114,6 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
             var myArray = new Array(document.getElementsByClassName("li--cad").length);
             for (x=0; x<document.getElementsByClassName("li--cad").length; x++) {
                 myArray[x] = document.getElementsByClassName("li--cad")[x].innerText.search(fileformat);
-                console.log[myArray];
             };
 
             //append classname to appropriate array elements for selected download format
@@ -81,9 +124,20 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
                 };
             };
 
-            var savefile = document.getElementsByClassName("button-save--stacked button-tertiary--cad HideOnItmPrint save-cad")[0];
-            setTimeout(function() {savefile.click();}, 100);
-            sendResponse({wrapup: "finished"});          
+            //Collect information for renaming the CAD file
+            var partNumber = document.getElementsByClassName("bk attrComp")[0].innerText;
+            var mainTitle = document.getElementsByClassName("header-primary--pd")[0].innerText;
+            if (document.getElementsByClassName("header-secondary--pd")[0] == null) {
+                subTitle = "";
+            }
+            else {
+                var subTitle = document.getElementsByClassName("header-secondary--pd")[0].innerText;
+            };
+            var fileformatArray = ["3-D EDRW", "3-D IGES", "3-D SAT", "3-D Solidworks", "3-D STEP", "2-D DWG", "2-D DXF", "2-D Solidworks"];
+            var fileExtensionArray = [".edrw", ".igs", ".sat", ".sldprt", ".step", ".dwg", ".dxf", ".slddrw"];
+            var fileExtension = fileExtensionArray[fileformatArray.indexOf(fileformat)];
+            var renamedFile = mainTitle + " " + subTitle + ", Part " + partNumber +  fileExtension;
+            sendResponse({wrapup: "prepared", newfilename: renamedFile});      
         };
     };    
 });
